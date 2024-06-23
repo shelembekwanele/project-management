7fcf26d098e3a0566396f949290805be57caf0ae
Kwanele Shelembe <kwaneleshelembe845@gmail.com>
2024-06-23 19:24:17 +0200

    added elements to dashboard

--- resources/js/Pages/Dashboard.jsx
+++ resources/js/Pages/Dashboard.jsx
@@ -1,5 +1,10 @@
+import DangerButton from '@/Components/DangerButton';
+import InputLabel from '@/Components/InputLabel';
+import PrimaryButton from '@/Components/PrimaryButton';
+import TextInput from '@/Components/TextInput';
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout';
 import { Head } from '@inertiajs/react';
+import { IoIosSearch } from 'react-icons/io';
 
 export default function Dashboard({ auth }) {
     return (
@@ -11,8 +16,16 @@ export default function Dashboard({ auth }) {
 
             <div className="py-12">
                 <div className="max-w-7xl mx-auto sm:px-6 lg:px-8">
-                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                        <div className="p-6 text-gray-900">You're logged in!</div>
+                    <div className="bg-white overflow-hidden shadow-sm sm:rounded-lg p-5 flex items-end justify-between">
+                        <div className='m-5'>
+                            <InputLabel className='py-2'>Search for project.</InputLabel>
+                            <div>
+                              <TextInput/> 
+                            </div>
+                            
+                        </div>
+                        
+                       <PrimaryButton className='mb-6'>Add new Project</PrimaryButton>
                     </div>
                 </div>
             </div>
